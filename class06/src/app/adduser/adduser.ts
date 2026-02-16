aee919840c3a2ede74a5d34fa3dedd4e0325b9fc
manahilabid <soniaaid806@gmail.com>
2026-02-16 11:24:58 -0700

    create , read , delete

---
+++ class06/src/app/adduser/adduser.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adduser',
+  imports: [],
+  templateUrl: './adduser.html',
+  styleUrl: './adduser.css',
+})
+export class Adduser {
+
+}
